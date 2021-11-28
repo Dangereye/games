74ca069fbb8b82fb630430abb7faf161b8683d48
Craig Puxty <craig_puxty@talktalk.net>
2021-11-28 14:20:02 +0000

    Added additional platform SVGs - all scaled to 300 X 300 with Illustrator

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,98 @@
 import Image from "next/image";
 
 function Home({ data }) {
-  const { results } = data;
-  console.log(results);
+  // const { results } = data;
+  // console.log(results);
   return (
     <section className="section" id="all-games">
       <div className="container">
         <h2 className="page-title">Games</h2>
         <div className="grid grid--multiple">
-          {results.map((game) => (
+          <Image
+            src="/svg/platforms/android.svg"
+            width="300"
+            height="300"
+            alt="Android"
+          />
+          <Image
+            src="/svg/platforms/neo-geo.svg"
+            width="300"
+            height="300"
+            alt="Neo Geo"
+          />
+          <Image
+            src="/svg/platforms/3do.svg"
+            width="300"
+            height="300"
+            alt="3DO"
+          />
+          <Image
+            src="/svg/platforms/amiga.svg"
+            width="300"
+            height="300"
+            alt="Amiga"
+          />
+          <Image
+            src="/svg/platforms/sega.svg"
+            width="300"
+            height="300"
+            alt="Sega"
+          />
+          <Image
+            src="/svg/platforms/atari.svg"
+            width="300"
+            height="300"
+            alt="Atari"
+          />
+          <Image
+            src="/svg/platforms/ios.svg"
+            width="300"
+            height="300"
+            alt="IOS"
+          />
+          <Image
+            src="/svg/platforms/linux.svg"
+            width="300"
+            height="300"
+            alt="Linux"
+          />
+          <Image
+            src="/svg/platforms/mac.svg"
+            width="300"
+            height="300"
+            alt="Mac"
+          />
+          <Image
+            src="/svg/platforms/nintendo.svg"
+            width="300"
+            height="300"
+            alt="Nintendo"
+          />
+          <Image
+            src="/svg/platforms/pc.svg"
+            width="300"
+            height="300"
+            alt="PC"
+          />
+          <Image
+            src="/svg/platforms/playstation.svg"
+            width="300"
+            height="300"
+            alt="Playstation"
+          />
+          <Image
+            src="/svg/platforms/web.svg"
+            width="300"
+            height="300"
+            alt="Web"
+          />
+          <Image
+            src="/svg/platforms/xbox.svg"
+            width="300"
+            height="300"
+            alt="Xbox"
+          />
+          {/* {results.map((game) => (
             <div className="game game__card" key={game.slug}>
               <div className="game__image">
                 <Image
@@ -26,8 +110,9 @@ function Home({ data }) {
                   {game.parent_platforms.map((item) => (
                     <Image
                       src={`/svg/platforms/${item.platform.slug}.svg`}
-                      width="20"
-                      height="20
+                      width="200"
+                      height="200
+
                       "
                       alt={item.platform.name}
                       title={item.platform.slug}
@@ -37,7 +122,7 @@ function Home({ data }) {
                 <div>{game.name}</div>
               </div>
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
     </section>
@@ -46,16 +131,16 @@ function Home({ data }) {
 
 export default Home;
 
-export async function getStaticProps() {
-  const response = await fetch(
-    `https://api.rawg.io/api/games?key=${process.env.API_KEY}`,
-    {
-      method: "GET",
-      mode: "no-cors",
-      headers: { "Content-Type": "application/json" },
-    }
-  );
-  const data = await response.json();
+// export async function getStaticProps() {
+//   const response = await fetch(
+//     `https://api.rawg.io/api/games?key=${process.env.API_KEY}`,
+//     {
+//       method: "GET",
+//       mode: "no-cors",
+//       headers: { "Content-Type": "application/json" },
+//     }
+//   );
+//   const data = await response.json();
 
-  return { props: { data } };
-}
+//   return { props: { data } };
+// }
