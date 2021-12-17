5f4edc8ce7060ed555a41252b764db4194c6cc89
Craig Puxty <craig_puxty@talktalk.net>
2021-12-17 22:21:08 +0000

    Updated platformId to use gameCards

--- pages/platforms/[platformId].js
+++ pages/platforms/[platformId].js
@@ -1,67 +1,20 @@
-import { useEffect, useContext } from "react";
-import { ThemeContext } from "../../contexts/ThemeContext";
+import { useContext } from "react";
 import { AppContext } from "../../contexts/AppContext";
-import useStatus from "../../hooks/useStatus";
-import useClientFetch from "../../hooks/useClientFetch";
 import Head from "next/head";
-import GameCard from "../../components/shared/GameCard";
-import Button from "../../components/shared/Button";
-import Loader from "../../components/shared/Loader";
+import GameCards from "../../components/shared/GameCards";
 
 function GamesList({ data, status }) {
-  const { themeState } = useContext(ThemeContext);
   const { appState } = useContext(AppContext);
-  const validateStatus = useStatus();
-  const addGames = useClientFetch();
-  // console.log("Platform Data: ", data);
-
-  const fetchMore = () => {
-    addGames(appState.data.next);
-  };
-
-  useEffect(() => {
-    validateStatus(data, status);
-  }, [data]);
 
   return (
     <>
       <Head>
-        <title>{data.seo_title}</title>
+        <title>{appState.data.seo_title}</title>
         <meta name="author" content="Craig Puxty" />
         <meta name="keywords" content={appState.data.seo_keywords} />
         <meta name="description" content={appState.data.seo_description} />
       </Head>
-      <section className="section" style={{ color: themeState.text.primary }}>
-        <div className="container">
-          {appState.isLoading ? (
-            <Loader />
-          ) : appState.error.isError ? (
-            <>
-              <h1 className="page-title">
-                Network error{appState.error.status}.
-              </h1>
-              <p>{appState.error.message}.</p>
-            </>
-          ) : (
-            <>
-              <h1 className="page-title">{appState.data.seo_h1}</h1>
-              <div className="grid grid--multiple">
-                {appState.data.results.map((game) => (
-                  <GameCard game={game} key={game.id} />
-                ))}
-              </div>
-              {appState.isLoadingMore && <Loader />}
-              {appState.data.next && (
-                <Button
-                  name="Load More"
-                  styles="btn--large btn--accent btn--center"
-                  func={fetchMore}
-                />
-              )}
-            </>
-          )}
-        </div>
-      </section>
+      <GameCards data={data} status={status} />
     </>
   );
 }
