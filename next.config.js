bbe9aa2b0ca3addd841331229f9ec096258d7f7a
Craig Puxty <craig_puxty@talktalk.net>
2022-01-09 19:01:18 +0000

    Added additional image domain to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["media.rawg.io", "steamcdn-a.akamaihd.net"],
+    domains: ["media.rawg.io", "steamcdn-a.akamaihd.net", "img.itch.zone"],
   },
 };
