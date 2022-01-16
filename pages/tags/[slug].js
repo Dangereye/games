bbffb734c6c659a1816a725dc2d03e5b7e921317
Craig Puxty <craig_puxty@talktalk.net>
2022-01-16 19:26:54 +0000

    Added ordering filters to tags route

--- pages/tags/[slug].js
+++ pages/tags/[slug].js
@@ -1,17 +1,23 @@
 import { useEffect, useContext } from "react";
 import { AppContext } from "../../contexts/AppContext";
 import useStatus from "../../hooks/useStatus";
+import useFilters from "../../hooks/useFilters";
 import Head from "next/head";
 import GameCards from "../../components/shared/game_cards/GameCards";
 
 function Tags({ initial }) {
   const { appState } = useContext(AppContext);
   const validateStatus = useStatus();
+  const { handleFilters, asPath, filter } = useFilters();
   console.log("Genres: ", initial);
 
   useEffect(() => {
     validateStatus(initial);
   }, [initial]);
+
+  useEffect(() => {
+    handleFilters();
+  }, [asPath, filter]);
 
   return (
     <>
@@ -28,9 +34,10 @@ function Tags({ initial }) {
 export default Tags;
 
 export async function getServerSideProps(context) {
-  const { params } = context;
+  const { params, query } = context;
+  const ordering = query.ordering ? `&ordering=${query.ordering}` : "";
   const res = await fetch(
-    `https://api.rawg.io/api/games?tags=${params.slug}&page_size=40&filter=true&comments=true&key=${process.env.API_KEY}`,
+    `https://api.rawg.io/api/games?tags=${params.slug}&page_size=40&filter=true&comments=true&key=${process.env.API_KEY}${ordering}`,
     {
       method: "GET",
       mode: "no-cors",
