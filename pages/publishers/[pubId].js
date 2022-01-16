74cc34693ae676b1b164fd04bcda78e47892bd12
Craig Puxty <craig_puxty@talktalk.net>
2022-01-16 19:16:26 +0000

    Added ordering filters to publishers & search routes

--- pages/publishers/[pubId].js
+++ pages/publishers/[pubId].js
@@ -3,15 +3,21 @@ import { AppContext } from "../../contexts/AppContext";
 import Head from "next/head";
 import useStatus from "../../hooks/useStatus";
 import GameCards from "../../components/shared/game_cards/GameCards";
+import useFilters from "../../hooks/useFilters";
 function Publishers({ pub, games }) {
   const { appState } = useContext(AppContext);
   const validateStatus = useStatus();
+  const { handleFilters, asPath, filter } = useFilters();
   console.log("Publisher: ", pub);
   console.log("Games: ", games);
 
   useEffect(() => {
     validateStatus(games);
   }, [games]);
+
+  useEffect(() => {
+    handleFilters();
+  }, [asPath, filter]);
 
   return (
     <>
@@ -29,7 +35,8 @@ function Publishers({ pub, games }) {
 export default Publishers;
 
 export async function getServerSideProps(context) {
-  const { params } = context;
+  const { params, query } = context;
+  const ordering = query.ordering ? `&ordering=${query.ordering}` : "";
   const options = {
     method: "GET",
     mode: "no-cors",
@@ -42,7 +49,7 @@ export async function getServerSideProps(context) {
       options
     ),
     fetch(
-      `https://api.rawg.io/api/games?publishers=${params.pubId}&key=${process.env.API_KEY}`,
+      `https://api.rawg.io/api/games?publishers=${params.pubId}&key=${process.env.API_KEY}${ordering}`,
       options
     ),
   ]);
--- pages/search/[slug].js
+++ pages/search/[slug].js
@@ -1,15 +1,21 @@
 import { useEffect } from "react";
+import useStatus from "../../hooks/useStatus";
+import useFilters from "../../hooks/useFilters";
 import Head from "next/head";
 import GameCards from "../../components/shared/game_cards/GameCards";
-import useStatus from "../../hooks/useStatus";
 
 function SearchDetails({ initial, slug }) {
   const validateStatus = useStatus();
+  const { handleFilters, asPath, filter } = useFilters();
   console.log("Search: ", initial);
 
   useEffect(() => {
     validateStatus(initial);
   }, [initial]);
+
+  useEffect(() => {
+    handleFilters();
+  }, [asPath, filter]);
 
   return (
     <>
@@ -27,9 +33,10 @@ function SearchDetails({ initial, slug }) {
 export default SearchDetails;
 
 export async function getServerSideProps(context) {
-  const { params } = context;
+  const { params, query } = context;
+  const ordering = query.ordering ? `&ordering=${query.ordering}` : "";
   const res = await fetch(
-    `https://api.rawg.io/api/games?key=${process.env.API_KEY}&search=${params.slug}&page_size=40`,
+    `https://api.rawg.io/api/games?key=${process.env.API_KEY}&search=${params.slug}&page_size=40${ordering}`,
     {
       method: "GET",
       mode: "no-cors",
