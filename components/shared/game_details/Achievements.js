f002a2be965672b47532606f56cb4953d4ab9057
Craig Puxty <craig_puxty@talktalk.net>
2022-02-19 17:42:33 +0000

    Refactored Achievements to fetch all available achievements.

--- components/shared/game_details/Achievements.js
+++ components/shared/game_details/Achievements.js
@@ -1,15 +1,34 @@
-import { useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { ThemeContext } from "../../../contexts/ThemeContext";
 import Image from "next/image";
 import Button from "../buttons/Button";
 
 function Achievements({ achievements }) {
   const { themeState } = useContext(ThemeContext);
+  const [count] = useState(achievements.count);
+  const [list, setList] = useState(achievements.results);
+  const [page, setPage] = useState(achievements.next);
   const [limit, setLimit] = useState(true);
 
   const toggleAmount = () => {
     setLimit(!limit);
   };
+
+  useEffect(() => {
+    const fetchMore = async () => {
+      if (list.length < count) {
+        const res = await fetch(page, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+        const data = await res.json();
+        setPage(data.next);
+        setList([...list, ...data.results]);
+      }
+    };
+    fetchMore();
+  }, [list]);
+
   return (
     <>
       {achievements.count > 0 && (
@@ -18,43 +37,46 @@ function Achievements({ achievements }) {
             Achievements
           </h4>
           <div className="achievements__list">
-            {(limit
-              ? achievements.results.filter((item, index) => index < 4)
-              : achievements.results
-            ).map((item, index) => (
-              <div
-                className="achievement"
-                key={`achievement-${index}`}
-                style={{ backgroundColor: themeState.background.secondary }}
-              >
-                <div className="achievement__image">
-                  <Image
-                    src={item.image ? item.image : ""}
-                    width="75"
-                    height="75"
-                    alt={item.name}
-                    placeholder="blur"
-                    blurDataURL={item.image ? item.image : ""}
-                  />
-                </div>
+            {(limit ? list.filter((item, index) => index < 4) : list).map(
+              (item, index) => (
+                <div
+                  className="achievement"
+                  key={`achievement-${index}`}
+                  style={{ backgroundColor: themeState.background.secondary }}
+                >
+                  <div className="achievement__image">
+                    <Image
+                      src={
+                        item.image ? item.image : "/images/missing-image.jpg"
+                      }
+                      width="75"
+                      height="75"
+                      alt={item.name}
+                      placeholder="blur"
+                      blurDataURL={
+                        item.image ? item.image : "/images/missing-image.jpg"
+                      }
+                    />
+                  </div>
 
-                <div className="achievement__details">
-                  <div
-                    className="achievement__percent"
-                    style={{ color: themeState.text.secondary }}
-                  >
-                    {item.percent}%
-                  </div>
-                  <div className="achievement__name">{item.name}</div>
-                  <div
-                    className="achievement__description"
-                    style={{ color: themeState.text.tertiary }}
-                  >
-                    {item.description}
+                  <div className="achievement__details">
+                    <div
+                      className="achievement__percent"
+                      style={{ color: themeState.text.secondary }}
+                    >
+                      {item.percent}%
+                    </div>
+                    <div className="achievement__name">{item.name}</div>
+                    <div
+                      className="achievement__description"
+                      style={{ color: themeState.text.tertiary }}
+                    >
+                      {item.description}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              )
+            )}
           </div>
           {achievements.results.length > 4 && (
             <Button
