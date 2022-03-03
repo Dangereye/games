df417e3c61fbf90d4e0e7b25edc8fc2931bd4623
Craig Puxty <craig_puxty@talktalk.net>
2022-03-03 15:31:51 +0000

    Updated tags dynamic route.

--- pages/tags/[slug].js
+++ pages/tags/[slug].js
@@ -1,26 +1,18 @@
 import useStatus from "../../hooks/useStatus";
 import useFilters from "../../hooks/useFilters";
-import Head from "next/head";
 import PageTemplate from "../../components/shared/PageTemplate";
 import Filters from "../../components/shared/filters/Filters";
 import GameCards from "../../components/shared/game_cards/GameCards";
 
-function Tags({ all, filtered }) {
-  const {} = useStatus(filtered);
-  const {} = useFilters("Tags");
+function Tags({ data }) {
+  const {} = useStatus(data);
+  const {} = useFilters();
 
   return (
-    <>
-      <Head>
-        <title>{all.seo_title}</title>
-        <meta name="keywords" content={all.seo_keywords} />
-        <meta name="description" content={all.seo_description} />
-      </Head>
-      <PageTemplate title={all.seo_h1}>
-        <Filters years={all.filters.years} genres={all.filters.genres} />
-        <GameCards title="Games List" />
-      </PageTemplate>
-    </>
+    <PageTemplate>
+      <Filters />
+      <GameCards title="Games List" />
+    </PageTemplate>
   );
 }
 export default Tags;
@@ -36,21 +28,13 @@ export async function getServerSideProps(context) {
   const dates = query.dates ? `&dates=${query.dates}` : "";
   const genres = query.genres ? `&genres=${query.genres}` : "";
 
-  let [all, filtered] = await Promise.all([
-    fetch(
-      `https://api.rawg.io/api/games?key=${process.env.API_KEY}&tags=${params.slug}&filter=true&comments=true`,
-      options
-    ),
-    fetch(
-      `https://api.rawg.io/api/games?key=${process.env.API_KEY}&tags=${params.slug}&page_size=40&${ordering}${dates}${genres}`,
-      options
-    ),
-  ]);
-
-  all = await all.json();
-  filtered = await filtered.json();
+  const res = await fetch(
+    `https://api.rawg.io/api/games?key=${process.env.API_KEY}&tags=${params.slug}&page_size=40&filter=true&comments=true${ordering}${dates}${genres}`,
+    options
+  );
+  const data = await res.json();
 
   return {
-    props: { all, filtered },
+    props: { data },
   };
 }
