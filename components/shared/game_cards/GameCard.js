f174f27b4af8bccb8b68bf3bb7b023a79e1eecfb
Craig Puxty <craig_puxty@talktalk.net>
2022-03-11 20:01:07 +0000

    Updated GameCard to improve accessibility.

--- components/shared/game_cards/GameCard.js
+++ components/shared/game_cards/GameCard.js
@@ -1,60 +1,55 @@
 import { useContext, forwardRef } from "react";
 import { ThemeContext } from "../../../contexts/ThemeContext";
-import { AppContext } from "../../../contexts/AppContext";
-import { useRouter } from "next/router";
+import Link from "next/link";
 import Image from "next/image";
 import ParentPlatforms from "../ParentPlatforms";
 import MetacriticComponent from "../MetacriticComponent";
 import DateComponent from "../DateComponent";
 
 export default forwardRef(function GameCard({ game }, ref) {
-  const { appDispatch } = useContext(AppContext);
   const { themeState } = useContext(ThemeContext);
-  const router = useRouter();
-
-  const gameDetails = (id) => {
-    appDispatch({ type: "LOADING" });
-    router.push(`/game/${id}`);
-  };
 
   return (
-    <div
-      ref={ref}
-      className="game-card"
-      style={{
-        color: themeState.text.tertiary,
-        backgroundColor: themeState.background.secondary,
-      }}
-      onClick={() => gameDetails(game.slug)}
-    >
-      <div className="game-card__image">
-        <Image
-          src={
-            game.background_image
-              ? game.background_image
-              : "/images/missing-image-game-card.jpg"
-          }
-          layout="fill"
-          sizes="17vw"
-          alt={game.name ? game.name : "Unknown game image."}
-        />
-      </div>
-      <div className="game-card__body">
-        <div
-          className="game-card__header"
-          style={{ fill: themeState.text.secondary }}
-        >
-          <ParentPlatforms platforms={game.parent_platforms} limit />
-          <MetacriticComponent score={game.metacritic} />
+    <Link href={`/game/${game.slug}`} ref={ref}>
+      <a
+        className="game-card"
+        style={{
+          color: themeState.text.tertiary,
+          backgroundColor: themeState.background.secondary,
+        }}
+      >
+        <div className="game-card__image">
+          <Image
+            src={
+              game.background_image
+                ? game.background_image
+                : "/images/missing-image-game-card.jpg"
+            }
+            layout="fill"
+            sizes="17vw"
+            alt={game.name ? game.name : "Unknown game image."}
+          />
         </div>
+        <div className="game-card__body">
+          <div
+            className="game-card__header"
+            style={{ fill: themeState.text.secondary }}
+          >
+            <ParentPlatforms platforms={game.parent_platforms} limit />
+            <MetacriticComponent score={game.metacritic} />
+          </div>
 
-        <div className="card-title" style={{ color: themeState.text.primary }}>
-          <span className="text">
-            {game.name ? game.name : "Untitled Game"}
-          </span>
+          <div
+            className="card-title"
+            style={{ color: themeState.text.primary }}
+          >
+            <span className="text">
+              {game.name ? game.name : "Untitled Game"}
+            </span>
+          </div>
+          <DateComponent date={game.released} />
         </div>
-        <DateComponent date={game.released} />
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 });
