e7adf1dfda22ad4d9d0a3a43fdd0dfa678e28eae
Craig Puxty <craig_puxty@talktalk.net>
2022-03-20 10:43:57 +0000

    Updated ESRBRating image.

--- components/game_details/header/ESRBRating.js
+++ components/game_details/header/ESRBRating.js
@@ -7,12 +7,14 @@ function ESRBRating({ esrb }) {
   return (
     <section className="esrb-rating" style={{ color: themeState.text.primary }}>
       <h2 className="section-title hidden">ESRB age rating</h2>
-      <Image
-        src={esrb ? `/svg/esrb/${esrb.slug}.svg` : "/svg/esrb/pending.svg"}
-        width="100"
-        height="150"
-        alt={esrb ? esrb.slug : "Pending"}
-      />
+      <div className="esrb-rating__image">
+        <Image
+          src={esrb ? `/svg/esrb/${esrb.slug}.svg` : "/svg/esrb/pending.svg"}
+          layout="fill"
+          sizes="17vw"
+          alt={esrb ? esrb.slug : "Pending"}
+        />
+      </div>
     </section>
   );
 }
