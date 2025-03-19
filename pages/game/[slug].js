f5d593905afa46ed7c3e3664cfd34e869f409e70
Craig Puxty <craig_puxty@talktalk.net>
2025-03-19 16:45:22 +0000

    Redeploy

--- pages/game/[slug].js
+++ pages/game/[slug].js
@@ -1,21 +1,21 @@
-import { useContext } from "react";
-import { AppContext } from "../../contexts/AppContext";
-import { ThemeContext } from "../../contexts/ThemeContext";
-import useUpdateState from "../../hooks/useUpdateState";
-import Loader from "../../components/shared/Loader";
-import ErrorMessage from "../../components/shared/ErrorMessage";
-import Head from "next/head";
-import InfoBanner from "../../components/game_details/header/InfoBannner";
-import Trailer from "../../components/game_details/header/Trailer";
-import Description from "../../components/game_details/header/Description";
-import Team from "../../components/game_details/header/Team";
-import ESRBRating from "../../components/game_details/header/ESRBRating";
-import Screenshots from "../../components/game_details/header/Screenshots";
-import Stats from "../../components/game_details/stats/Stats";
-import Stores from "../../components/game_details/Stores";
-import Specification from "../../components/game_details/Specification";
-import GameCards from "../../components/shared/game_cards/GameCards";
-import Achievements from "../../components/game_details/Achievements";
+import { useContext } from 'react';
+import { AppContext } from '../../contexts/AppContext';
+import { ThemeContext } from '../../contexts/ThemeContext';
+import useUpdateState from '../../hooks/useUpdateState';
+import Loader from '../../components/shared/Loader';
+import ErrorMessage from '../../components/shared/ErrorMessage';
+import Head from 'next/head';
+import InfoBanner from '../../components/game_details/header/InfoBannner';
+import Trailer from '../../components/game_details/header/Trailer';
+import Description from '../../components/game_details/header/Description';
+import Team from '../../components/game_details/header/Team';
+import ESRBRating from '../../components/game_details/header/ESRBRating';
+import Screenshots from '../../components/game_details/header/Screenshots';
+import Stats from '../../components/game_details/stats/Stats';
+import Stores from '../../components/game_details/Stores';
+import Specification from '../../components/game_details/Specification';
+import GameCards from '../../components/shared/game_cards/GameCards';
+import Achievements from '../../components/game_details/Achievements';
 
 function GameDetails({
   initial,
@@ -36,10 +36,10 @@ function GameDetails({
     <>
       <Head>
         <title>Games | {game.name}</title>
-        <meta name="author" content="Craig Puxty" />
+        <meta name='author' content='Craig Puxty' />
         <meta
-          name="description"
-          content="Video game database and discovery service - powered by RAWG.IO"
+          name='description'
+          content='Video game database and discovery service - powered by RAWG.IO'
         />
       </Head>
       {appState.isLoading ? (
@@ -51,12 +51,12 @@ function GameDetails({
           {!appState.modal.isOpen && (
             <>
               <header
-                className="game-details grid"
+                className='game-details grid'
                 style={{ color: themeState.text.primary }}
               >
                 {/* Left */}
                 <div>
-                  <h1 className="display-title">{game.name}</h1>
+                  <h1 className='display-title'>{game.name}</h1>
                   <InfoBanner game={game} />
                   <Trailer trailers={trailers.results} />
                   <Description description={game.description_raw} />
@@ -80,23 +80,23 @@ function GameDetails({
               <GameCards
                 condition={additions.count > 0}
                 title="Editions, DLC's and  apps"
-                subtitle="Game additions"
+                subtitle='Game additions'
                 list={additions.results}
                 limited
               />
 
               <GameCards
                 condition={series.count > 0}
-                title="Games in the series"
-                subtitle="Game series"
+                title='Games in the series'
+                subtitle='Game series'
                 list={series.results}
                 limited
               />
 
               <GameCards
                 condition={parent.count > 0}
-                title="Parent game"
-                subtitle="Addition to"
+                title='Parent game'
+                subtitle='Addition to'
                 list={parent.results}
                 limited
               />
@@ -114,9 +114,9 @@ export default GameDetails;
 export async function getServerSideProps(context) {
   const { params } = context;
   const options = {
-    method: "GET",
-    mode: "no-cors",
-    headers: { "Content-Type": "application/json" },
+    method: 'GET',
+    mode: 'no-cors',
+    headers: { 'Content-Type': 'application/json' },
   };
 
   let [
